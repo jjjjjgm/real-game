2f909728b5a4fa8d2bc20e8471ff9a47f3f1b5a3
jarglmaa <jagamaasaikhan@gmail.com>
2023-05-05 08:12:17 +0800

    Saihan ywjnoo

---
+++ app.js
@@ -0,0 +1,11 @@
+var activePlayer = 1;
+
+var scores=[0,0];
+
+var roundScore=0;
+
+var dice= Math.floor((Math.random()*6))+1;
+
+console.log('Шоо'+dice);
+
+window.document.querySelector("#score-0").textContent="100";
