0d154e5898f7d2dc7988b8bb399ece51818c2ddc
jarglmaa <jagamaasaikhan@gmail.com>
2023-05-05 11:41:12 +0800

    d

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var scores=[0,0];
 
 var roundScore=0;
 
-var dice= Math.floor((Math.random()*6))+1;
 
-console.log('Шоо'+dice);
+window.document.querySelector("#score-0").textContent="0";
+window.document.querySelector("#score-1").textContent="0";
+window.document.querySelector("#current-0").textContent="0";
+window.document.querySelector("#current-1").textContent="0";
 
-window.document.querySelector("#score-0").textContent="100";
+
+var diceDom= document.querySelector(".dice");
+
+window.document.querySelector(".btn-roll").addEventListener('click', function (){
+    var diceNumber= Math.floor((Math.random()*6))+1;
+
+    window.document.querySelector(".dice").style.display="block";
+    document.querySelector(".dice").src="dice-"+diceNumber+".png";
+});
+
