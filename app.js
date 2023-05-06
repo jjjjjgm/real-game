9ab4036e2980790a11475fa19f52b9d4bfeea2e5
jarglmaa <jagamaasaikhan@gmail.com>
2023-05-06 11:28:15 +0800

    toglogchiin eeljig solidg bolsn

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var activePlayer = 1;
+var activePlayer = 0;
 
 var scores=[0,0];
 
@@ -12,11 +12,27 @@ window.document.querySelector("#current-1").textContent="0";
 
 
 var diceDom= document.querySelector(".dice");
-
+diceDom.style.display="none";
 window.document.querySelector(".btn-roll").addEventListener('click', function (){
     var diceNumber= Math.floor((Math.random()*6))+1;
 
-    window.document.querySelector(".dice").style.display="block";
+    diceDom.style.display="block";
     document.querySelector(".dice").src="dice-"+diceNumber+".png";
+
+    if (diceNumber!=1){
+        // 1 ees ylgaatai too ywuulna.
+        roundScore=roundScore+diceNumber;
+        document.getElementById("current-"+activePlayer).textContent=roundScore;
+    }
+    else{
+        roundScore=0;
+        document.getElementById("current-"+activePlayer).textContent=0;
+        //1 buuwal toglogchiim eeljig solino
+        activePlayer ===0 ? (activePlayer=1) : (activePlayer=0);
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+        diceDom.style.display="none";
+    
+    }
 });
 
